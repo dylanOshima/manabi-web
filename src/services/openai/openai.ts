85e6a93222f6e51771697584b61a6832d009e510
dylanOshima <dylanoshima@gmail.com>
2023-05-08 14:26:43 +0100

    Adds response typing to openAI request

--- src/services/openai/openai.ts
+++ src/services/openai/openai.ts
@@ -1,11 +1,14 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, CreateCompletionResponse, OpenAIApi } from "openai";
+import {AxiosResponse} from "axios";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export const sendPromptToOpenAI = async (prompt: string) => {
+export const sendPromptToOpenAI = async (
+  prompt: string
+): Promise<AxiosResponse<CreateCompletionResponse, any>> => {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt,
