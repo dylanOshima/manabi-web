de0a593ce3c16ff3f9ac1386b4de84b433d24d39
dylanOshima <dylanoshima@gmail.com>
2023-05-27 20:53:38 +0100

    Create util for casting and typing ids

--- src/consts/ids.ts
+++ src/consts/ids.ts
@@ -1,3 +1,5 @@
+import { isNil, toNumber } from "lodash";
+
 /**
  * General purpose ID type.
  */
@@ -8,3 +10,15 @@ export type ID = number;
  * @returns a six digit ID
  */
 export const genID = (): ID => Math.floor(100000 + Math.random() * 900000);
+
+/**
+ * A value that is potentially an ID.
+ * @param val a value that is maybe an ID
+ * @returns an ID or null
+ */
+export function maybeID(val: any): ID | null {
+  if (isNil(val) || !isFinite(val)) {
+    return null;
+  }
+  return toNumber(val) as ID;
+}
