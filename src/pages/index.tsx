fd440c12f25f3fa0a9eb6fc0adec1ede0754d660
dylanOshima <dylanoshima@gmail.com>
2023-06-15 10:55:44 +0800

    Update quick links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
 import Page from "@/components/Page";
 import ModulesWidget from "@/components/widgets/ModulesWidget";
-import { Button } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const Index = () => (
   <Page>
-    <NextLink
-      href={`/course`}
-      passHref>
-      <Button aria-label={"Button to view courses"}>Courses Page</Button>
-    </NextLink>
-    <NextLink
-      href={`/study`}
-      passHref>
-      <Button aria-label={"Button to start a study session"}>Study Page</Button>
-    </NextLink>
+    <Flex direction='row'>
+      <NextLink
+        href={`/course`}
+        passHref>
+        <Button
+          aria-label={"Button to view courses"}
+          mr={2}>
+          Courses Page
+        </Button>
+      </NextLink>
+      <NextLink
+        href={`/study`}
+        passHref>
+        <Button
+          aria-label={"Button to start a study session"}
+          mr={2}>
+          Study Page
+        </Button>
+      </NextLink>
+    </Flex>
     <ModulesWidget />
   </Page>
 );
